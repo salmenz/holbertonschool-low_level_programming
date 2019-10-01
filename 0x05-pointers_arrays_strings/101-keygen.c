9320e903b8f06ec584a98a131734a4c53c9e7673
Salmen <salmen.zooro@gmail.com>
2019-10-01 21:37:14 +0000

    fix adv

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - entry point in c
+ * main - fdjf
  *
- * Return: 0 if successful
+ * Return: 0 if succ
  */
 int main(void)
 {
-char c = 0;
+char s = 0;
 int i = 0;
 char j = 0;
 srand(time(0));
 while (i <= 2645)
 {
-c = rand() % 128;
-i += c;
-putchar(c);
+s = rand() % 128;
+i += s;
+putchar(s);
 }
 j = 2772 - i;
 putchar(j);
