c1a0690d8dbd7b9da82b5b95a87258817b1f94e4
Salmen <salmen.zooro@gmail.com>
2019-10-03 15:54:36 +0000

    com

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _strncat - Returns the length of a string
+ * @dest: The string whose length will be returned
+ * @src : String
+ * @i : integer
+ * Return: The length of the string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int k = 0;
+
+while ((src[k] != '\0') && (k < n))
+{
+*(dest + k) = *(src + k);
+k++;
+}
+
+while (k < n)
+{
+*(dest + k) = '\0';
+k++;
+}
+return (dest);
+}
