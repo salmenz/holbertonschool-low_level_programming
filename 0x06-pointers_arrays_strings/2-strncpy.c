cc0688d6f8408d00ab839c376e193665c07078fc
Salmen <salmen.zooro@gmail.com>
2019-10-03 16:28:58 +0000

    co

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+int k = 0;
 
-               for (i = 0; i < n && src[i] != '\0'; i++)
-                   dest[i] = src[i];
-               for ( ; i < n; i++)
-                   dest[i] = '\0';
-
-               return dest;
+while ((*(src + k) != '\0') && (k < n))
+{
+*(dest + k) = *(src + k);
+k++;
+}
+while (k < n)
+{
+*(dest + k) = '\0';
+k++;
+}
+return (dest);
 }
