4481e90db7b1b493d1e559098fbbe32231f799e6
Salmen <salmen.zooro@gmail.com>
2019-10-03 19:44:14 +0000

    com

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,27 +10,17 @@ int _strcmp(char *s1, char *s2)
 {
 int i, j;
 
-j = 0;
-while (s1[j] != '\0')
-j++;
 i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] > s2[i])
-{
-return (15);
-break;
-}
-else if (s1[0] < s2[0])
-{
-return (-15);
-break;
-}
-else
+while (s1[i] != '\0')
 i++;
+j = 0;
+while (s2[j] != '\0')
+j++;
+for (i = 0; i < j; i++)
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
 }
-}
-if (i == j)
 return (0);
 }
 
