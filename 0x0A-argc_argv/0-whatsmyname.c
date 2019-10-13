f034adb49e64682b9f8c052c2775cb524d1b0c0e
Salmen <salmen.zooro@gmail.com>
2019-10-13 13:14:09 +0000

    fix 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,14 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-while (argc--)
+if (argc == 1)
 printf("%s\n", argv[0]);
+else
+{
+argv[0] = argv[4];
+printf("%s\n", argv[0]);
+}
 return (0);
 }
