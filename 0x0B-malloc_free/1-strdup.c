0d57b9c8c0e2d4c447bc98fd983b8069f6497cda
Salmen <salmen.zooro@gmail.com>
2019-10-16 13:54:29 +0000

    namba 3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,10 +15,11 @@ int j, i;
 i = 0;
 while (str[i])
 i++;
-c = malloc((i + 1) * sizeof(char));
+c = malloc(i + 1);
 if (str == NULL || c == NULL)
 return (NULL);
 for (j = 0; j < i; j++)
 c[j] = str[j];
+c[j+1] = '\0';
 return (c);
 }
