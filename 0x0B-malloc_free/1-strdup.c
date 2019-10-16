f11ab2994e349a67789c76dbe325b721554c1070
Salmen <salmen.zooro@gmail.com>
2019-10-16 13:02:20 +0000

    numera doue

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - Returns a pointer to a new string
+ * @str: The string
+ *
+ * Return: Pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+char *c;
+int j, i;
+
+i = 0;
+while (str[i])
+i++;
+c = malloc(i + 1);
+if (str == NULL || c == NULL)
+return (NULL);
+for (j = 0; j < i; j++)
+c[j] = str[j];
+c[j] = '\0';
+return (c);
+free(c);
+}
