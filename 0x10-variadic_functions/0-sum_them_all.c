f054c8031760c05a4abc0199b219562c9e2ba4f5
Salmen <salmen.zooro@gmail.com>
2019-10-26 13:57:17 +0000

    fix0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,11 +8,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i, sum;
+unsigned int i;
+int sum = 0;
 va_list l;
 
 va_start(l, n);
-sum = 0;
+if (n == 0)
+return (0);
 for (i = 0; i < n; i++)
 sum += va_arg(l, int);
 va_end(l);
