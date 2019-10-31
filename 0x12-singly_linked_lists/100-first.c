13ff0fbc1bb837c0196eabe0d47401bff6bc5215
Salmen <salmen.zooro@gmail.com>
2019-10-31 21:50:41 +0000

    adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * first - print the string before the main is executed
+ *
+ */
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
