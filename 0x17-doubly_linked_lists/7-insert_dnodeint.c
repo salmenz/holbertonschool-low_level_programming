fab3adf337ea71514cf0a3fc941e398bcc4cc38c
Salmen Zouari <salmen.zooro@gmail.com>
2019-12-15 13:56:35 +0000

    fixxx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,50 +2,49 @@
 #include "lists.h"
 /**
  * insert_dnodeint_at_index - insert a node in nth idx at a double linked list
- * @head: the first node in the list
+ * @h: the first node in the list
  * @idx: the index of the node added
  * @n: the integer to insert into the data node
  * Return: node
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx,
-int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *p, *new;
 unsigned int i = 0;
+dlistint_t *new;
+dlistint_t *c = *h;
+dlistint_t *e = *h;
 
-p = *head;
-if (head == NULL && idx != 0)
-return (NULL);
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
+if (*h == NULL && idx > 0)
+return (NULL);
+while (e->next != NULL)
+e = e->next;
 new->n = n;
 if (idx == 0)
 {
-new->next = *head;
-p->next->prev = new;
-new->prev = NULL;
-*head = new;
+add_dnodeint(h, n);
 return (new);
 }
-while (i < idx - 1)
+while ((i < idx - 1) && (c != NULL))
 {
-p = p->next;
-if (p == NULL)
-{
-free(new);
-return (NULL);
-}
+c = c->next;
 i++;
 }
-if (p->next != NULL)
+if (c == e)
 {
-new->next = p->next;
-new->next->prev = new;
-}
-else
-new->next = NULL;
-new->prev = p;
-p->next = new;
+add_dnodeint_end(h, n);
 return (new);
+}
+else if (c != NULL)
+{
+new->next = c->next;
+new->prev = c;
+if (c->next != NULL)
+c->next->prev = new;
+c->next = new;
+return (new);
+}
+return (NULL);
 }
